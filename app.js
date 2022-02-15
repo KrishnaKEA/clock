bbbb38a05846c5173917db4734304be7069bc44a
krishnaamen <krishnaament@gmail.com>
2022-02-15 15:47:59 +0100

    new

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
 	res.sendFile(`${__dirname}/public/index.html`);
 });
-const port = Process.env.PORT || 3000;
-app.listen(port, (err) => {
-	console.log("Listening to the server at port number 8081");
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, (err) => {
+	console.log(`Listening to the server at port number ${PORT}`);
 });
