745aaba932bcf277c84143688c62da4586a82f3c
krishnaamen <krishnaament@gmail.com>
2022-02-15 16:45:39 +0100

    updates

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+// importing modules
 const express = require("express");
 const app = express();
+// it allows us to use external css and javascript to the html file using express.
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
 	res.sendFile(`${__dirname}/public/index.html`);
 });
+
+// line below is required while deploying
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, (err) => {
 	console.log(`Listening to the server at port number ${PORT}`);
--- public/script.js
+++ public/script.js
@@ -1,16 +1,38 @@
+//function to calculate time(hours,minutes and seconds)
 const getTime = () => {
 	let date = new Date();
 	let hour = date.getHours();
 	let minute = date.getMinutes();
 	let second = date.getSeconds();
 	let ampm = hour >= 12 ? "PM" : "AM";
-
 	minute == minute < 10 ? "0" + minute : minute;
 	second == second < 10 ? "0" + second : second;
 	return `${hour} : ${minute} : ${second}  : ${ampm} `;
 };
-
+// function to calculate date and name of the day.
+const getDateAndDay = () => {
+	const now = new Date();
+	const dayNames = [
+		"Monday",
+		"Thuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+		"Sunday",
+	];
+	const daydiv = document.querySelector(".day");
+	const day = now.getDay();
+	daydiv.innerHTML = `${dayNames[day - 1]}`;
+	const timediv = document.querySelector(".date");
+	timediv.innerHTML = `${now.getFullYear()}/${now.getMonth() + 1}/${
+		now.getDate() + 1
+	}`;
+};
+// updating the date and time in each second
 setInterval(() => {
-	let timeObject = document.querySelector(".time");
+	const timeObject = document.querySelector(".time");
 	timeObject.innerHTML = getTime();
+	const d = new Date();
+	getDateAndDay();
 }, 1000);
